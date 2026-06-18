7c3d9dd0b864a2e3e8e41ba6d120b736fe194876
Divor <todjropatrice2002@gmail.com>
2026-06-18 09:34:42 +0000

    chore: remove Tauri/React/Rust implementation for Win32 C++ rewrite

--- src-tauri/src/worker/net.rs
+++
@@ -1,6 +0,0 @@
-pub const UA: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-     (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
-
-pub fn client() -> reqwest::Result<reqwest::Client> {
-    reqwest::Client::builder().user_agent(UA).build()
-}
